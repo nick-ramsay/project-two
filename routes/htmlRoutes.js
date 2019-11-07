9e85fbae2f23df7f5baa2cb77c5d4e020e801fb1
Nick Ramsay <NickRamsay1989@gmail.com>
2019-11-07 21:41:46 +1100

    Adding mechanicslogin.handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,10 @@ module.exports = function(app) {
     res.render("index");
   });
 
+  app.get("/login", function(req, res) {
+    res.render("mechaniclogin");
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
