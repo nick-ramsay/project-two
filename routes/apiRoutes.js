bf4acdfb1c711fde50106156771f94930056cc4b
aaa_alex <alexwu7583@gmail.com>
2019-11-09 14:00:37 +1100

    latest alex

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,140 @@
 var db = require("./connection.js");
+var moment = require("moment");
 
 module.exports = function(app) {
-  app.get("/api/mechanics", function(req, res) {
-    db.query("SELECT * FROM Mechanics", function(error, results) {
-      console.log(results);
+  app.get("/api/services", function(req, res) {
+    db.query("SELECT * FROM Services", function(error, results) {
+      res.json(results);
+    });
+  });
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  app.get("/api/mechaniccentres", function(req, res) {
+    db.query("SELECT * FROM MechanicCentres", function(error, results) {
+      res.json(results);
+    });
+  });
+  app.get("/api/mechaniccentres/:id", function(req, res) {
+    db.query(
+      "SELECT * FROM MechanicCentres WHERE ?",
+      { id: req.params.id },
+      function(error, results) {
+        res.json(results);
+      }
+    );
+  });
+  app.post("/api/mechaniccentres", function(req, res) {
+    var currDateTime = new moment();
+    db.query(
+      "INSERT INTO MechanicCentres (centre_name, phone, email, user_username, user_password, address_street, address_city, address_postcode, address_state, address_country, latitude, longitude, employee_count, created_date, created_time, createdAt, updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+      [
+        "ppp's auto repairs",
+        "0410500100",
+        "brian@gmail.com",
+        "brian@gmail.com",
+        "asdf1234",
+        "1 E Street",
+        "Eee City",
+        "2000",
+        "NSW",
+        "Australia",
+        10.1,
+        10.1,
+        3,
+        currDateTime.format("YYYY-MM-DD"),
+        currDateTime.format("HH:mm:ss"),
+        currDateTime.format("YYYY-MM-DD HH:mm:ss"),
+        currDateTime.format("YYYY-MM-DD HH:mm:ss")
+      ],
+      function(error, results) {
+        res.json(results);
+      }
+    );
+  });
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  app.get("/api/mechaniccenterservices", function(req, res) {
+    db.query("SELECT * FROM MechanicCentreServices", function(error, results) {
+      res.json(results);
+    });
+  });
+  app.get("/api/mechaniccenterservices", function(req, res) {
+    db.query("SELECT * FROM MechanicCentreServices", function(error, results) {
+      res.json(results);
+    });
+  });
+  app.post("/api/mechaniccenterservices", function(req, res) {
+    var currDateTime = new moment();
+    db.query("INSERT INTO MechanicCentres () VALUES (?, ?)", [], function(
+      error,
+      results
+    ) {
+      res.json(results);
+    });
+  });
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  app.get("/api/mechaniccenterordinaryhours", function(req, res) {
+    db.query("SELECT * FROM MechanicCentreOrdinaryHours", function(
+      error,
+      results
+    ) {
+      res.json(results);
+    });
+  });
+  app.get("/api/mechaniccenterordinaryhours/:mechanicid", function(req, res) {
+    db.query(
+      "SELECT * FROM MechanicCentreOrdinaryHours WHERE ?",
+      { mechanicid: req.params.id },
+      function(error, results) {
+        res.json(results);
+      }
+    );
+  });
+  app.post("/api/mechaniccenterservices", function(req, res) {
+    var currDateTime = new moment();
+    db.query("INSERT INTO MechanicCentres () VALUES (?, ?)", [], function(
+      error,
+      results
+    ) {
+      res.json(results);
+    });
+  });
+
+  //////////////////////////////////////////////////////////////////////////////
+
+  app.get("/api/appointments", function(req, res) {
+    db.query("SELECT * FROM Appointments", function(error, results) {
+      res.json(results);
+    });
+  });
+  app.get("/api/appointments/customer/:customerid", function(req, res) {
+    db.query(
+      "SELECT * FROM Appointments WHERE ?",
+      { customerid: req.params.id },
+      function(error, results) {
+        res.json(results);
+      }
+    );
+  });
+  app.get("/api/appointments/mechanic/:mechanicid", function(req, res) {
+    db.query(
+      "SELECT * FROM Appointments WHERE ?",
+      { mechanicid: req.params.id },
+      function(error, results) {
+        res.json(results);
+      }
+    );
+  });
+  app.post("/api/mechaniccenterservices", function(req, res) {
+    var currDateTime = new moment();
+    db.query("INSERT INTO MechanicCentres () VALUES (?, ?)", [], function(
+      error,
+      results
+    ) {
       res.json(results);
     });
   });
