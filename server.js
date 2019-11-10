6e34ca1f028ff0cae7c4609d42dce5fef8bc1a6d
aaa_alex <alexwu7583@gmail.com>
2019-11-10 20:35:18 +1100

    save

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
