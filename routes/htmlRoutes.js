77ec8f5af1065958da5b908fcdd7809b4451db7c
Nick Ramsay <NickRamsay1989@gmail.com>
2019-11-12 08:53:07 +1100

    Adding final ALEX2 revisions

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,7 +4,6 @@ module.exports = function(app) {
     res.render("index");
   });
 
-<<<<<<< HEAD
   app.get("/login", function(req, res) {
     res.render("mechaniclogin");
   });
@@ -19,31 +18,6 @@ module.exports = function(app) {
 
   app.get("/schedule", function(req, res) {
     res.render("mechanicschedule");
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-||||||| merged common ancestors
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-=======
-  app.get("/login", function(req, res) {
-    res.render("mechaniclogin");
-  });
-
-  app.get("/create-account", function(req, res) {
-    res.render("mechanicaccountcreate");
-  });
-
-  app.get("/account", function(req, res) {
-    res.render("mechanicaccount");
-  });
-
-  app.get("/schedule", function(req, res) {
-    res.render("mechanicschedule");
->>>>>>> b968364aa065165aee44000d9423283b5cb0d236
   });
 
   //customer inputs requests
