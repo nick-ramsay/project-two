d290e7d99816f84f3941473ec7e48d4bc41ac4ea
aaa_alex <alexwu7583@gmail.com>
2019-11-11 14:18:52 +1100

    merging views

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,28 @@
-// var db = require("./connection.js");
-
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
     res.render("index");
   });
 
-  // mechanics dashboard page
-  app.get("/profile/:mechaniccentreid", function(req, res) {
-    res.render("mechanicprofile");
+  app.get("/login", function(req, res) {
+    res.render("mechaniclogin");
+  });
+
+  app.get("/create-account", function(req, res) {
+    res.render("mechanicaccountcreate");
+  });
+
+  app.get("/account", function(req, res) {
+    res.render("mechanicaccount");
+  });
+
+  app.get("/schedule", function(req, res) {
+    res.render("mechanicschedule");
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
   });
 
   //customer inputs requests
@@ -25,9 +39,36 @@ module.exports = function(app) {
   app.get("/recmechanic", function(req, res) {
     res.render("mechanicdetails");
   });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
 };
+
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     res.render("index");
+//   });
+
+//   // mechanics dashboard page
+//   app.get("/profile/:mechaniccentreid", function(req, res) {
+//     res.render("mechanicprofile");
+//   });
+
+//   //customer inputs requests
+//   app.get("/requests", function(req, res) {
+//     res.render("brokendetails");
+//   });
+
+//   //customer inpts contact details
+//   app.get("/contactdetails", function(req, res) {
+//     res.render("customerdetails");
+//   });
+
+//   //server gives recommended mechanics
+//   app.get("/recmechanic", function(req, res) {
+//     res.render("mechanicdetails");
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
